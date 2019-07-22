9a68fec4fcac9c2d276edf157342ebc30cdca611
Sreeram Padmanabhan <sreeram@Sreerams-MacBook-Air.local>
2019-07-22 03:55:38 +0100

    chore: first 2 questions

--- src/data/app/questions.js
+++ src/data/app/questions.js
@@ -1,13 +1,22 @@
+import React from 'react';
+
+import '../../../assets/crypts/you.jpg';
+
 export default [
   {},
   {
+    question: <img src="/img/you.jpg" style={{ width: 200 }} />,
+    answer: '639bae9ac6b3e1a84cebb7b403297b79',
+    clues: [ 'Workforce', 'Whats he pointing at?' ],
+  },
+  {
     question: 'Dbqjubm pg Fohmboe',
-    answer: 'a363b8d13575101a0226e8d0d054f2e7',
+    answer: 'bc180dbc583491c00f8a1cd134f7517b',
     clues: [ 'Cipher', 'Letter shift' ],
   },
   {
-    question: "Where kings and queens contend..",
-    answer: 'a363b8d13575101a0226e8d0d054f2e7',
+    question: "Where kings and queens contend.",
+    answer: '53fb40c0dfc82831d472d01bb651216c',
     clues: [ '64^2', 'Black & White' ],
   },
 ]
