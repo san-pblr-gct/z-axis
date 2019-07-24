5c9a89861fb49292b0f3915d67bfc968810b5030
Sreeram Padmanabhan <sreeram.com@gmail.com>
2019-07-24 08:17:17 +0100

    chore: paybutton style update

--- src/components/PayButton/PayButton.js
+++ src/components/PayButton/PayButton.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-magic-numbers */
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import HourGlassIcon from '@material-ui/icons/HourglassFull'
+import { withStyles } from '@material-ui/core/styles';
+
+import styles from './styles';
 
 class Paytmbutton extends Component {
   state = {
@@ -10,12 +15,13 @@ class Paytmbutton extends Component {
     this.setState({
       submit: true,
     });
+    document.getElementById('pay-form').submit();
   }
   render() {
-
+    const { classes } = this.props;
     const { url, orderId, merchantId, website, industryTypeId, channelId, customerId, amount, phone, email, callbackUrl, checksum } = this.props;
 
-    return <form style={{ marginBottom: 0 }} className="form-control" action={url} name="f1" method="POST">
+    return <form id="pay-form" style={{ marginBottom: 0 }} className="form-control" action={url} name="f1" method="POST">
       <input type="hidden" name="MID" value={merchantId} />
       <input type="hidden" name="WEBSITE" value={website} />
       <input type="hidden" name="INDUSTRY_TYPE_ID" value={industryTypeId} />
@@ -27,15 +33,20 @@ class Paytmbutton extends Component {
       <input type="hidden" name="EMAIL" value={email} />
       <input type="hidden" name="CALLBACK_URL" size="64" value={callbackUrl} />
       <input type="hidden" name="CHECKSUMHASH" value={checksum} />
-      <Button variant="contained" color="secondary" style={{ margin: '0 auto', display: 'block', padding: 0, width: '100%', height: 44 }} disabled={this.state.submit}>
+      <Fab className={classes.fab} color={'secondary'} onClick={this.onSubmit.bind(this)} aria-label="clues" name="clues">
+        {this.state.submit
+          ? <HourGlassIcon />
+          : `Clues ${this.props.amount.toLocaleString('en-GB', { style: 'currency', currency: 'INR' })}`}
+      </Fab>
+      {/* <Button variant="contained" color="secondary" style={{ margin: '0 auto', display: 'block', padding: 0, width: '100%', height: 44 }} disabled={this.state.submit}>
         <input type="submit" style={{ outline: 'none', color: 'white', padding: 0, background: 'none', border: 'none', width: '100%', height: 44, fontSize: 14, textTransform: 'uppercase', fontWeight: 'normal' }} value={
           this.state.submit
             ? 'Please wait..'
             : `Reveal Clues: ${this.props.amount.toLocaleString('en-GB', { style: 'currency', currency: 'INR' })}`}
-        onClick={this.onSubmit.bind(this)} />
-      </Button>
+          onClick={this.onSubmit.bind(this)} />
+      </Button> */}
     </form>;
   }
 }
 
-export default Paytmbutton;
+export default withStyles(styles)(Paytmbutton);
---
+++ src/components/PayButton/styles.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-magic-numbers */
+
+export default () => ({
+  fab: {
+    height: 60,
+    width: 60,
+    position: 'fixed',
+    bottom: 10,
+    left: 'calc(50% - 30px)',
+    marginLeft: 5,
+  },
+});
