4378b443354ab7eea3b50d355be827beadc715e5
Sreeram Padmanabhan <sreeram@Sreerams-MacBook-Air.local>
2019-08-18 05:45:59 +0530

    fix: ttle fix

--- src/config/links.js
+++ src/config/links.js
@@ -3,8 +3,8 @@ module.exports = [
     title: 'Home',
     links: [
       {
-        title: 'Z Axis | Online Treasure Hunt',
-        ogTitle: 'Z Axis | Online Treasure Hunt',
+        title: 'Z Axis - Think outside the box',
+        ogTitle: 'Z Axis - Think outside the box',
         description: 'Do you think outside the box?',
         ogDescription: 'Z Axis is an Online Treasure Hunt App which will give you an online experience of a traditional treasure hunt. Your goal is to solve all the crypts.',
         ogImage: '/img/og_image.jpg',
