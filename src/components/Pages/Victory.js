b70ae5122c437441833f9364efcb33f4518973b8
Sreeram Padmanabhan <sreeram.com@gmail.com>
2019-08-18 07:03:56 +0530

    fix: victory message fix

--- src/components/Pages/Victory.js
+++ src/components/Pages/Victory.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 // import ListItem from '@material-ui/core/ListItem';
@@ -5,7 +6,7 @@ import List from '@material-ui/core/List';
 import { Typography } from '@material-ui/core';
 import Fab from '@material-ui/core/Fab';
 import ShareIcon from '@material-ui/icons/Share';
-import Reset from '@material-ui/icons/Refresh';
+// import Reset from '@material-ui/icons/Refresh';
 import moment from 'moment';
 // import classNames from 'classnames';
 
@@ -15,21 +16,20 @@ class Victory extends Component {
   render() {
     const { startTime, endTime } = this.props;
     const time = totalTime(startTime, endTime);
-    // eslint-disable-next-line no-magic-numbers
     const percent = Math.ceil(time / 6) + 1;
-    const shareMessage = `I have successfully completed the Z Axis Challenge in ${time} hours. Its your turn.`;
+    const shareMessage = `I have successfully completed the Z Axis Challenge in under ${time + 1} ${time ? 'hour' : 'hours'}. Its your turn now.`;
 
     return <div>
       <List dense>
         <img style={{ width: 200, margin: '10px auto', display: 'block' }} src={'/img/victory.gif'} />
         <Typography variant="h6" style={{ textAlign: 'center', margin: 10 }}>Congratulations!</Typography>
-        <Typography variant="subtitle1" style={{ textAlign: 'center', margin: 30 }}>{`You have completed the Z Axis Challenge. It took you ${time} hours to complete this. You are in the top ${percent}% of the people who have cleared this.`}</Typography>
-        <Typography style={{ textAlign: 'center', margin: '20px 20px 50px 20px' }}>Now its your turn to pass on the challenge.</Typography>
-        <div style={{ width: 160, margin: 'auto' }}>
-          <Fab style={{ margin: 10 }} color={'primary'} onClick={() => this.props.handleRefreshOpen()} aria-label="share" name="share">
+        <Typography variant="subtitle1" style={{ textAlign: 'center', margin: 30 }}>{`You have completed the Z Axis Challenge. It took you just under ${time + 1} ${time ? 'hour' : 'hours'} to complete this. And that means you are in the top ${percent}% of the people who have cleared this.`}</Typography>
+        <Typography style={{ textAlign: 'center', margin: 30 }}>Now its your turn to pass on the challenge.</Typography>
+        <div style={{ width: 76, margin: 'auto' }}>
+          {/* <Fab style={{ margin: 10 }} color={'primary'} onClick={() => this.props.handleRefreshOpen()} aria-label="share" name="share">
             <span className="hidden-accessiiblity">Reset</span>
             <Reset />
-          </Fab>
+          </Fab> */}
           <Fab style={{ margin: 10 }} color={'primary'} onClick={() => this.props.onShare(shareMessage)} aria-label="share" name="share">
             <ShareIcon />
           </Fab>
