01d59352d6c99acc072c98078047fb415e6479a1
Sreeram Padmanabhan <sreeram.com@gmail.com>
2019-08-18 06:13:11 +0530

    chore: victory message formatting

--- src/components/Pages/Victory.js
+++ src/components/Pages/Victory.js
@@ -9,22 +9,21 @@ import Reset from '@material-ui/icons/Refresh';
 import moment from 'moment';
 // import classNames from 'classnames';
 
-const totalTime = (s, e) => moment(s).fromNow(e);
-
-// const MyListItem = ({ primary = null, secondary = null }) => <ListItem variant="subtitle1">
-//   <ListItemText primary={primary} secondary={secondary} />
-// </ListItem>;
+const totalTime = (s, e) => moment(e).diff(s, 'hours');
 
 class Victory extends Component {
   render() {
     const { startTime, endTime } = this.props;
     const time = totalTime(startTime, endTime);
+    // eslint-disable-next-line no-magic-numbers
+    const percent = Math.ceil(time / 6);
 
     return <div>
       <List dense>
         <img style={{ width: 200, margin: '10px auto', display: 'block' }} src={'/img/victory.gif'} />
-        <Typography variant="h6" style={{ textAlign: 'center', margin: '10px' }}>Congratulations!</Typography>
-        <Typography variant="subtitle1" style={{ textAlign: 'center', margin: '10px' }}>{`You have completed all the levels successfully! You truly do think out of the box!! It took you ${time} to win this!`}</Typography>
+        <Typography variant="h6" style={{ textAlign: 'center', margin: 10 }}>Congratulations!</Typography>
+        <Typography variant="subtitle1" style={{ textAlign: 'center', margin: '30px 20px 50px' }}>{`You have completed all the levels of Z Axis successfully. It took you ${time} hours to complete this. You are in the top ${percent}% of the people who have cleared all levels.`}</Typography>
+        {/* <Typography style={{ textAlign: 'center', margin: '10px' }}>Go ahead and set this as screenshot.</Typography> */}
         <div style={{ width: 160, margin: 'auto' }}>
           <Fab style={{ margin: 10 }} color={'primary'} onClick={() => this.props.handleRefreshOpen()} aria-label="share" name="share">
             <span className="hidden-accessiiblity">Reset</span>
